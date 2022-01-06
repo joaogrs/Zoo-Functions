5069ded9bfdc5283e0dd87f6cc85c4fdb6184592
jaogrs <jgabrielrodrigues2@gmail.com>
2022-01-06 16:19:13 -0300

    requisito 1 lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const {species} = data;
-  const animals = []; /*array vazio*/
-  ids.forEach((id) => 
-  animals.push(species.find((animal) => animal.id === id)));
+  const { species } = data;
+  const animals = []; /* array vazio */
+  ids.forEach((id) =>
+    animals.push(species.find((animal) => animal.id === id)));
   console.log(animals);
   return animals;
-};
+}
 
 module.exports = getSpeciesByIds;
