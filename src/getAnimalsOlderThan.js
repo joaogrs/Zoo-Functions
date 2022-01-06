7c8d135357d51d801ee78f2daf0a113f01f2790d
jaogrs <jgabrielrodrigues2@gmail.com>
2022-01-06 17:01:40 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const selectedAnimal = species.find((specie) => specie.name === animal);
+  const {residents} = selectedAnimal;
+  return residents.filter((year) => year.age > age).length === residents.length;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,9 +3,12 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   const { species } = data;
   const animals = []; /* array vazio */
+
   ids.forEach((id) =>
     animals.push(species.find((animal) => animal.id === id)));
+    
   console.log(animals);
+
   return animals;
 }
 
