32903fefecb524df80d53358eeeb71846e73268c
jaogrs <jgabrielrodrigues2@gmail.com>
2022-01-07 14:48:59 -0300

    requisito 4 lint 2

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,9 +3,8 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   const { employees } = data;
   const vazio = {};
-
-  const selectedEmployer = employees.find((employer) => {
-    employer.firstName === employeeName || employer.lastName === employeeName});
+  const func = (employer) => employer.firstName === employeeName || employer.lastName === employeeName 
+  const selectedEmployer = employees.find(func);
 
   if (!employeeName) {
     return vazio;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  const  { employees } = data;
+  const { employees } = data;
   const manager = [];
   employees.forEach((employee) => manager.push(employee.managers));
   const managersClean = manager.reduce((arr, element) => arr.concat(element), []);
@@ -9,12 +9,15 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
-  const  { employees } = data;
+  const { employees } = data;
   const responsibleFor = [];
-    if (isManager(managerId) === true) {
+  if (isManager(managerId) === true) {
     const responsibleFor2 = employees.filter((employee2) => {
-  employee2.managers.includes(managerId)});
-    responsibleFor2.forEach((employee3) => responsibleFor.push(`${employee3.firstName} ${employee3.lastName}`));
+      return employee2.managers.includes(managerId);
+    });
+    responsibleFor2.forEach((employee3) => {
+      responsibleFor.push(`${employee3.firstName} ${employee3.lastName}`)
+    });
   return responsibleFor;
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
