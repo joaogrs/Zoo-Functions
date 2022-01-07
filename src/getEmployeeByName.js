35d53e370400f41b1df134ca6eacdfddeb4ef7f4
jaogrs <jgabrielrodrigues2@gmail.com>
2022-01-07 14:33:35 -0300

    requisito 4 lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,8 +3,9 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   const { employees } = data;
   const vazio = {};
-  
-  const selectedEmployer = employees.find((employer) => employer.firstName === employeeName || employer.lastName === employeeName );
+
+  const selectedEmployer = employees.find((employer) => {
+    employer.firstName === employeeName || employer.lastName === employeeName});
 
   if (!employeeName) {
     return vazio;
