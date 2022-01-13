4da7bfcc1cc29f0706214dbe674bbd6c13a188cd
jaogrs <jgabrielrodrigues2@gmail.com>
2022-01-13 14:21:18 -0300

    requisito 9 lint 1

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,53 +1,55 @@
-const data = require('../data/zoo_data');
+const data = require('../data/zoo_data'); 
 const { employees } = data;
 
-const getNameAnimalByID = (id) => {
-  return data.species.find((specie) => specie.id === id).name;
-}
-const getLocationAnimalByID = (id) => {
-  return data.species.find((specie) => specie.id === id).location;
-}
-function consult (nameOrId) {
+const getNameAnimalByID = (id) => data.species.find((specie) => specie.id === id).name;
+
+const getLocationAnimalByID = (id) => data.species.find((specie) => specie.id === id).location;
+
+function consult(nameOrId) {
   const employeeObj = employees
-  .find((employee) => employee.id === nameOrId || employee.firstName === nameOrId || employee.lastName === nameOrId );
+    .find((employee) => employee.id === nameOrId 
+    || employee.firstName === nameOrId || employee.lastName === nameOrId);
   if (employeeObj === undefined) {
-    const objReturn = 0 ;
+    let objReturn;
+    return objReturn;
   } else {
-  const objReturn = {id: `${employeeObj.id}`, fullName: `${employeeObj.firstName} ${employeeObj.lastName}`};
-  const arr1 = [];
-  const arr2 = [];
-  employeeObj.responsibleFor.forEach((id1) => {
-    arr1.push(getNameAnimalByID(id1));
-    arr2.push(getLocationAnimalByID(id1));
-  });
-  objReturn['species'] = arr1;
-  objReturn['locations'] = arr2 ;
-  return objReturn;
-    };
-};
+    const objReturn = { id: `${employeeObj.id}`,
+     fullName: `${employeeObj.firstName} ${employeeObj.lastName}` };
+    const arr1 = [];
+    const arr2 = [];
+    employeeObj.responsibleFor.forEach((id1) => {
+      arr1.push(getNameAnimalByID(id1));
+      arr2.push(getLocationAnimalByID(id1));
+    });
+    objReturn.species = arr1;
+    objReturn.locations = arr2;
+    return objReturn;
+  }
+}
 const criaLista = () => {
   const list = [];
   employees.forEach((employee) => list.push(consult(employee.firstName)));
   return list;
-}
+};
 
 function getEmployeesCoverage(options = {}) {
   const verify = employees
-  .find((employee) => employee.id === options.id || employee.firstName === options.name|| employee.lastName === options.name);
+  .find((employee) => employee.id === options.id 
+  || employee.firstName === options.name || employee.lastName === options.name);
+  const verify2 = verify === undefined
 
-  if(!options.name && !options.id) {
+  if (!options.name && !options.id) {
     return criaLista();
-  }
-  if (verify === undefined) {
+  };
+  if (verify2) {
     throw new Error('Informações inválidas');
-  }
+  };
   if (options.name) {
     return consult(options.name);
-  }
+  };
   if (options.id) {
     return consult(options.id);
-  }
-}
+  };
+};
 
-console.log(getEmployeesCoverage())
 module.exports = getEmployeesCoverage;
